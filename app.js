8f9a6f7aa528b666a0b56119edf4b1a4b388cc58
Eni Puska <eni.puska@gmail.com>
2019-05-05 18:57:24 +0300

    ...

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = express.Router();
+app.use(express.static(__dirname + '/components'));
 
+router.get('/',function(req,res){
+    res.sendFile(__dirname + '/autoComplete/autoComplete.html');
+});
 app.listen(3000, function() {
     console.log("Server running on port 3000");
 });
