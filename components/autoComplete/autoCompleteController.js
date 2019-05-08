f4bb651d04ef2883f2fc5028e458aea925239c92
Eni Puska <eni.puska@gmail.com>
2019-05-09 00:28:06 +0300

    graphical changes responsive

--- components/autoComplete/autoCompleteController.js
+++ components/autoComplete/autoCompleteController.js
@@ -7,7 +7,22 @@ app.factory('liveSearchService', ['$http', function($http) {
     return factory;
 }]);
 app.controller('autoCompleteController', ['$scope','$http', 'liveSearchService', function($scope,$http, liveSearchService) {
-    
+    $scope.tabletVersion = false;
+    $scope.mobileVersion = false;
+    $(window).resize(function(){
+        $scope.$apply(function(){
+          if(window.innerWidth <= 770) {
+            $scope.tabletVersion = true;
+          } else {
+            $scope.tabletVersion = false;
+          }
+          if(window.innerWidth <= 480) {
+            $scope.mobileVersion = true;
+          } else {
+            $scope.mobileVersion = false;
+          }
+        });
+    });
     $scope.searchCountries = function(viewValue, searchValue) {
         return liveSearchService.getCountries(viewValue, searchValue).then(function(response) {
             return response.data.response.map(function(item) {
