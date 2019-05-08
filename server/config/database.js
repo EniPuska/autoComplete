214f3e203d4ff4df6bc96e7b446e6d040de11013
Eni Puska <eni.puska@gmail.com>
2019-05-08 13:40:01 +0300

    some graphical changes

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : '127.0.0.1',
     user     : 'test',
     password : 'test',
     database : 'autocomplete'
